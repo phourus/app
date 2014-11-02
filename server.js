3faac328bbf7c352f8ee06dbd8d7822939755ea3
Jesse Drelick <info@jessedrelick.com>
2014-11-01 18:16:38 -0700

    Server

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var React = require('react');
+var jsx = require('node-jsx').install();
+
+var fs = require('fs');
+
+var phourus = express();
+
+
+
+phourus.get('/stream', function(req, res){
+    var template = fs.readFile('build/template.html', 'utf8', function(err, out){
+        var component = renderComponent('home');
+        out = out.replace('$CONTENT', component);
+        out = out.replace('$SCRIPT', '<script type="text/javascript" src="react/home.js"></script>');
+        res.send(out, 200);
+    });   
+});
+
+function renderComponent(id){
+    var component = require('./build/react/' + id);
+    console.log(component);
+    return React.renderToString(component());    
+}
+
+phourus.use(express.static('./build'));
+phourus.listen(4567, function(){
+    console.log('server started');
+});
