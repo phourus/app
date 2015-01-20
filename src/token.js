544a4febf1c0f81027406d3f250efcba0656fbf8
Jesse Drelick <info@jessedrelick.com>
2015-01-19 21:01:17 -0800

    Misc

---
+++ src/token.js
@@ -0,0 +1,16 @@
+var token = {
+    get: function () {
+      var token = localStorage.getItem('token'); 
+      if (!token) {
+          token = false;
+      } 
+      return token;
+    },
+    save: function (token) {
+      localStorage.setItem('token', token);
+    },
+    remove: function () {
+      localStorage.removeItem('token');
+    }
+}
+module.exports = token;
