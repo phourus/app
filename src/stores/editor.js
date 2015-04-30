c3f34bbce0c8b94885ed03edff5541bfae201e38
Jesse Drelick <info@jessedrelick.com>
2015-04-29 23:19:37 -0700

    Updated Editor store

--- src/stores/editor.js
+++ src/stores/editor.js
@@ -26,12 +26,11 @@ let Editor = Reflux.createStore({
        self.trigger({posts: data.rows, total: data.total});
      });
     posts.on('single', function (code, data) {
-     console.log(data);
      if (code != 200) {
          msg('yellow', 'Post could not be loaded', code);
          return;
       }
-      self.trigger({post: data});
+      self.trigger(data);
     });
     posts.on('add', function (code, data) {
        if (code != 201) {
@@ -64,104 +63,103 @@ let Editor = Reflux.createStore({
   },
   _reset: function () {
     this.trigger({post: {}});
-  },
-});
-
-Editor.Tags = Reflux.createStore({
-  tags: [],
-  init: function () {
-    let self = this;
-    this.listenTo(Tags.collection, this._collection)
-    this.listenTo(Tags.add, this._add);
-    this.listenTo(Tags.remove, this._remove);
-
-    tags.on('collection', function (code, data) {
-        if (code != 200) {
-            msg('yellow', 'Tags could not be loaded', code);
-            return;
-        }
-        this.tags = data;
-        self.trigger({tags: this.tags});
-    });
-    tags.on('add', function (code, data) {
-        if (code != 201) {
-            msg('yellow', 'Tag could not be created', code);
-            return;
-        }
-        this.tags.push(data);
-        self.trigger({tags: this.tags});
-    });
-    tags.on('remove', function (code, data) {
-        if (code != 204) {
-            msg('yellow', 'Tag could not be removed', code);
-            return;
-        }
-    });
-  },
-  _collection: function (params) {
-    tags.collection(params);
-  },
-  _add: function (model) {
-    tags.add(model);
-  },
-  _remove: function (id) {
-    tags.remove(id);
   }
-});
+  /*,
+  Tags: Reflux.createStore({
+    tags: [],
+    init: function () {
+      let self = this;
+      this.listenTo(Tags.collection, this._collection)
+      this.listenTo(Tags.add, this._add);
+      this.listenTo(Tags.remove, this._remove);
 
-Editor.Links = Reflux.createStore({
-  links: [],
-  init: function () {
-    let self = this;
-    this.listenTo(Links.collection, this._collection)
-    this.listenTo(Links.add, this._add);
-    this.listenTo(Links.save, this._save);
-    this.listenTo(Links.remove, this._remove);
+      tags.on('collection', function (code, data) {
+          if (code != 200) {
+              msg('yellow', 'Tags could not be loaded', code);
+              return;
+          }
+          this.tags = data;
+          self.trigger({tags: this.tags});
+      });
+      tags.on('add', function (code, data) {
+          if (code != 201) {
+              msg('yellow', 'Tag could not be created', code);
+              return;
+          }
+          this.tags.push(data);
+          self.trigger({tags: this.tags});
+      });
+      tags.on('remove', function (code, data) {
+          if (code != 204) {
+              msg('yellow', 'Tag could not be removed', code);
+              return;
+          }
+      });
+    },
+    _collection: function (params) {
+      tags.collection(params);
+    },
+    _add: function (model) {
+      tags.add(model);
+    },
+    _remove: function (id) {
+      tags.remove(id);
+    }
+  }),
+  Links: Reflux.createStore({
+    links: [],
+    init: function () {
+      let self = this;
+      this.listenTo(Links.collection, this._collection)
+      this.listenTo(Links.add, this._add);
+      this.listenTo(Links.save, this._save);
+      this.listenTo(Links.remove, this._remove);
 
-    links.on('collection', function (code, data) {
-        if (code != 200) {
-            msg('yellow', 'Links could not be loaded', code);
-            return;
-        }
-        this.links = data;
-        self.trigger({links: this.links});
-    });
-    links.on('add', function (code, data) {
-        if (code != 201) {
-            msg('yellow', 'Link could not be created', code);
-            return;
-        }
-        this.links.push(data);
-        self.trigger({links: this.links});
-    });
-    links.on('save', function (code, data) {
-        if (code != 201) {
-            msg('yellow', 'Link could not be saved', code);
-            return;
-        }
-        this.links.push(data);
-        self.trigger({links: this.links});
-        msg('green', 'Link saved successfully', code);
-    });
-    links.on('remove', function (code, data) {
-        if (code != 204) {
-            msg('red', 'Link could not be removed', code);
-            return;
-        }
-    });
-  },
-  _collection: function (params) {
-    tags.collection(params);
-  },
-  _add: function (model) {
-    tags.add(model);
-  },
-  _save: function (id, model) {
-    tags.save(id, model);
-  },
-  _remove: function (id) {
-    tags.remove(id);
-  }
+      links.on('collection', function (code, data) {
+          if (code != 200) {
+              msg('yellow', 'Links could not be loaded', code);
+              return;
+          }
+          this.links = data;
+          self.trigger({links: this.links});
+      });
+      links.on('add', function (code, data) {
+          if (code != 201) {
+              msg('yellow', 'Link could not be created', code);
+              return;
+          }
+          this.links.push(data);
+          self.trigger({links: this.links});
+      });
+      links.on('save', function (code, data) {
+          if (code != 201) {
+              msg('yellow', 'Link could not be saved', code);
+              return;
+          }
+          this.links.push(data);
+          self.trigger({links: this.links});
+          msg('green', 'Link saved successfully', code);
+      });
+      links.on('remove', function (code, data) {
+          if (code != 204) {
+              msg('red', 'Link could not be removed', code);
+              return;
+          }
+      });
+    },
+    _collection: function (params) {
+      tags.collection(params);
+    },
+    _add: function (model) {
+      tags.add(model);
+    },
+    _save: function (id, model) {
+      tags.save(id, model);
+    },
+    _remove: function (id) {
+      tags.remove(id);
+    }
+  })*/
 });
 
 module.exports = Editor;
