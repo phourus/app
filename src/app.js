966b240efe391e477c7a51bca1b881c13c5ad2cd
Jesse Drelick <info@jessedrelick.com>
2015-06-06 17:03:37 -0700

    Remove Leaders button

--- src/app.js
+++ src/app.js
@@ -15,7 +15,6 @@ let App = React.createClass({
                     	<a href="./"></a>
                     </div>
                     <nav className="nav">
-                      <Link to="leaders" className="game fa fa-trophy"></Link>
                     	<Link to="search" className="search fa fa-search"></Link>
                     	<Link to="editor" className="editor fa fa-pencil"></Link>
                     	<Link to="account" className="account fa fa-user"><span className="notifications">13</span></Link>
