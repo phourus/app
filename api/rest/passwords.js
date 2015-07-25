3368a0f1ac9a3bc26886369916c00c1e6d1065e2
Jesse Drelick <info@jessedrelick.com>
2015-07-25 13:32:13 -0700

    PUT only for Passwords endpoint

--- api/rest/passwords.js
+++ api/rest/passwords.js
@@ -2,55 +2,15 @@ var router = require('express').Router();
 
 var passwords = require('../models/passwords');
 
-router.get('/:id', (req, res) => {
-  var id;
-  passwords.single(id)
-  .then(function (data) {
-      res.send(200, data);
-  })
-  .catch(function (err) {
-    console.error(err);
-    res.send(500);
-  });
-});
-router.get('', (req, res) => {
-  var params;
-  passwords.collection(params)
-  .then(function (data) {
-      res.send(200, data);
-  })
-  .catch(function (err) {
-    console.error(err);
-    res.send(500);
-  });
-});
-router.post('', (req, res) => {
-  var model;
-  passwords.create(model)
-  .then(function (data) {
-      res.send(201, data);
-  })
-  .catch(function (err) {
-    console.error(err);
-    res.send(500);
-  });
-});
-router.put('/:id', (req, res) => {
-  var id, model;
-  passwords.save(id, model)
+router.put('', (req, res) => {
+  if (!req.user_id) {
+    return res.send(401);
+  }
+  let model = req.body;
+  passwords.SESSION_USER = req.user_id;
+  passwords.save(model)
   .then(function (data) {
       res.send(204, data);
-  })
-  .catch(function (err) {
-    console.error(err);
-    res.send(500);
-  });
-});
-router.delete('/:id', (req, res) => {
-  var id;
-  passwords.remove(id)
-  .then(function (data) {
-      res.send(202, data);
   })
   .catch(function (err) {
     console.error(err);
