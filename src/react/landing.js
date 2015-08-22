53c86b4b58762fc000bb17d18c59c054b43cad06
Jesse Drelick <info@jessedrelick.com>
2015-08-22 12:31:34 -0700

    Slider images

--- src/react/landing.js
+++ src/react/landing.js
@@ -22,12 +22,50 @@ let Landing = React.createClass({
 });
 
 let Slider = React.createClass({
+  getInitialState: function () {
+    return {
+      slide: 0
+    }
+  },
   render: function () {
+    var classes = ['', '', '', '', ''];
+    classes[this.state.slide] = 'selected';
     return (
-      <div className="slider">
-        <img src="/assets/banner.jpg" className="banner" />
+      <div className="slider" onClick={this._next}>
+        <div className={classes[0]}>
+          <img src="/assets/landing/home.jpg" className="banner" />
+          <div className="title">The Pursuit of Truth</div>
+          <p>Phourus is about the Pursuit of Truth across the 4 Elements of Society.</p>
+        </div>
+        <div className={classes[1]}>
+          <img src="/assets/landing/world.jpg" className="banner" />
+          <div className="title">Economy & Environment</div>
+          <p>The balance between Economy & Environment is vital to the health & prosperity of a nation and it's people.</p>
+        </div>
+        <div className={classes[2]}>
+          <img src="/assets/landing/mind.jpg" className="banner" />
+          <div className="title">Knowledge & Education</div>
+          <p>An educated culture is one that can make effective decisions that improve the lives of many.</p>
+        </div>
+        <div className={classes[3]}>
+          <img src="/assets/landing/voice.jpg" className="banner" />
+          <div className="title">Government & Politics</div>
+          <p>Government exists to protect and serve, not to hinder or limit a society.</p>
+        </div>
+        <div className={classes[4]}>
+          <img src="/assets/landing/self.jpg" className="banner" />
+          <div className="title">Beliefs & Religion</div>
+          <p>Understanding who we are as individuals and why we exist in a vast universe of empty space.</p>
+        </div>
       </div>
     );
+  },
+  _next: function () {
+    if (this.state.slide === 4) {
+      this.setState({slide: 0});
+      return;
+    }
+    this.setState({slide: this.state.slide + 1});
   }
 });
 
@@ -99,37 +137,39 @@ let Create = React.createClass({
     return (
       <div className="create">
         <h2>Create</h2>
-        <div>
-          <i className="fa fa-laptop" /><br />
-          Blogs
-        </div>
-        <div>
-          <i className="fa fa-calendar" /><br />
-          Events
-        </div>
-        <div>
-          <i className="fa fa-puzzle-piece" /><br />
-          Subjects
-        </div>
-        <div>
-          <i className="fa fa-question" /><br />
-          Questions
-        </div>
-        <div>
-          <i className="fa fa-bullhorn" /><br />
-          Debates
-        </div>
-        <div>
-          <i className="fa fa-line-chart" /><br />
-          Polls
-        </div>
-        <div>
-          <i className="fa fa-road" /><br />
-          Beliefs
-        </div>
-        <div>
-          <i className="fa fa-quote-right" /><br />
-          Quotes
+        <div className="squeeze">
+          <div>
+            <i className="fa fa-laptop" /><br />
+            Blogs
+          </div>
+          <div>
+            <i className="fa fa-calendar" /><br />
+            Events
+          </div>
+          <div>
+            <i className="fa fa-puzzle-piece" /><br />
+            Subjects
+          </div>
+          <div>
+            <i className="fa fa-question" /><br />
+            Questions
+          </div>
+          <div>
+            <i className="fa fa-bullhorn" /><br />
+            Debates
+          </div>
+          <div>
+            <i className="fa fa-line-chart" /><br />
+            Polls
+          </div>
+          <div>
+            <i className="fa fa-road" /><br />
+            Beliefs
+          </div>
+          <div>
+            <i className="fa fa-quote-right" /><br />
+            Quotes
+          </div>
         </div>
       </div>
     );
@@ -141,21 +181,23 @@ let Compete = React.createClass({
     return (
       <div className="compete">
         <h2>Compete</h2>
-        <div>
-          <i className="fa fa-users" /><br />
-          Engage users
-        </div>
-        <div>
-          <i className="fa fa-bolt" /><br />
-          Earn Influence points
-        </div>
-        <div>
-          <i className="fa fa-line-chart" /><br />
-          Rise in rankings
-        </div>
-        <div>
-          <i className="fa fa-trophy" /><br />
-          Become a Leader
+        <div className="squeeze">
+          <div>
+            <i className="fa fa-users" /><br />
+            Engage users
+          </div>
+          <div>
+            <i className="fa fa-bolt" /><br />
+            Earn Influence points
+          </div>
+          <div>
+            <i className="fa fa-line-chart" /><br />
+            Rise in rankings
+          </div>
+          <div>
+            <i className="fa fa-trophy" /><br />
+            Become a Leader
+          </div>
         </div>
       </div>
     );
@@ -167,12 +209,14 @@ let Integrate = React.createClass({
     return (
       <div className="integrate">
         <h2>Integrate</h2>
-        <i className="fa fa-facebook" />
-        <i className="fa fa-dropbox" />
-        <i className="fa fa-linkedin" />
-        <i className="fa fa-slack" />
-        <i className="fa fa-google" />
-        <i className="fa fa-rss" />
+        <div className="squeeze">
+          <i className="fa fa-facebook" />
+          <i className="fa fa-dropbox" />
+          <i className="fa fa-linkedin" />
+          <i className="fa fa-slack" />
+          <i className="fa fa-google" />
+          <i className="fa fa-rss" />
+        </div>
       </div>
     );
   }
