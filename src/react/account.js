462b9f3ffc9a7413ac43f25545779a21693ff567
Jesse Drelick <info@jessedrelick.com>
2015-09-30 10:49:44 -0700

    USPS address validation

--- src/react/account.js
+++ src/react/account.js
@@ -9,6 +9,32 @@ let Login = require('./401').Login;
 let moment = require('moment');
 let ImageUploader = require('../pic');
 
+// _validate: function () {
+//   var self = this;
+//   var addresses = [];
+//   var xml = this._xmlSchema();
+//   $.get('http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=' + xml, function (xml) {
+//     //var data = $.parseXML(xml);
+//
+//     $('Address', xml).each(function (i) {
+//       var address = {};
+//       var err = $(this).find('Error');
+//       var message = $(this).find('ReturnText');
+//       if (err.length || message.length) {
+//         addresses.push({err: (err.find('Description').text() || message.text())});
+//         return;
+//       }
+//
+//       address.unit = $(this).find('Address1').text();
+//       address.street = $(this).find('Address2').text();
+//       address.city = $(this).find('City').text();
+//       address.state = $(this).find('State').text();
+//       address.zip = $(this).find('Zip5').text();
+//       addresses.push(address);
+//     });
+//     self.setState({validAddresses: addresses});
+//   });
+// },
 let Account = React.createClass({
   getInitialState: function () {
     return {
