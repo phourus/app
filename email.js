e5768926c6e670cfc68dc853a539856d7df60a66
Jesse Drelick <info@jessedrelick.com>
2015-11-06 16:50:14 -0800

    Email From name

--- email.js
+++ email.js
@@ -3,11 +3,13 @@ var footer = "Thank you,<br /><br />The Phourus Team<br />1411 7th St #305<br />
 var generate = function (template, data) {
   var templates = {
     reset: {
+      name: 'Phourus.com',
       from: 'info@phourus.com',
       subject: 'Password Reset',
       content: 'Hello, <p>We have received a request to reset your password on Phourus. If you did not submit this request, please let us know. Otherwise, simply click the link below to go to the password reset form.</p> <a href="http://phourus.com/?reset=true&token=' + data.token + '&userId=' + data.userId + '">Click here to reset your password</a><br /><br />' + footer
     },
     signup: {
+      name: 'Phourus.com',
       from: 'info@phourus.com',
       subject: 'Your New Phourus Account',
       content: 'Hello! <p>This email was sent to confirm you have successfully created your account on <a href="http://phourus.com">Phourus.com</a> using this email address. We are very excited to have you as a user and as part of the Phourus Platform! Please let us know if you need anything, we\'d love to help :)</p>' + footer
@@ -23,6 +25,7 @@ module.exports = function (template, data) {
     sendgrid.send({
       to:       data.email,
       from:     email.from,
+      fromname: email.name,
       subject:  email.subject,
       html:     email.content
     }, function(err, json) {
