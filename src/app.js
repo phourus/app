66a8c1c0632a1216f0f3519352aa689159162354
Jesse Drelick <info@jessedrelick.com>
2015-11-20 13:31:10 -0800

    Sidebar

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ let Profile = require('./react/profile');
 let App = React.createClass({
   getInitialState: function () {
     return {
+      sidebarVisible: false,
       tint: null
     };
   },
@@ -25,21 +26,22 @@ let App = React.createClass({
         <div className="spacer"></div>
         <Profile />
         <Alerts {...this.props.alerts} />
-        <div className="main">
+        <div className={this.state.sidebarVisible ? "main sidebarVisible" : "main"}>
+          <Sidebar sidebar={this._sidebar} sidebarVisible={this.state.sidebarVisible} />
           <div id="content">
             <RouteHandler />
           </div>
-          <footer className="footer">
-            <strong>1-844-PHOURUS</strong><br />
-            <span className="muted">(1-844-746-8787)</span><br />
-            <a href="mailto:info@phourus.com&Subject=">info@phourus.com</a><br /><br />
-            <span>© 2015 Phourus Inc. All Rights Reserved.</span><br />
-            <span className="muted">1411 7th St. #305, Santa Monica, CA 90401</span><br />
-            <Link to="terms" className="muted">Terms</Link> |
-            <Link to="privacy" className="muted">Privacy</Link>
-            <br clear="all" />
-          </footer>
         </div>
+        <footer className="footer">
+          <strong>1-844-PHOURUS</strong><br />
+          <span className="muted">(1-844-746-8787)</span><br />
+          <a href="mailto:info@phourus.com&Subject=">info@phourus.com</a><br /><br />
+          <span>© 2015 Phourus Inc. All Rights Reserved.</span><br />
+          <span className="muted">1411 7th St. #305, Santa Monica, CA 90401</span><br />
+          <Link to="terms" className="muted">Terms</Link> |
+          <Link to="privacy" className="muted">Privacy</Link>
+          <br clear="all" />
+        </footer>
       </div>
     );
   },
@@ -48,6 +50,45 @@ let App = React.createClass({
   },
   _tintOff: function () {
     this.setState({tint: null});
+  },
+  _sidebar: function () {
+    this.setState({sidebarVisible: !this.state.sidebarVisible});
+  }
+});
+
+let Sidebar = React.createClass({
+  render: function () {
+    let tags = [
+      {name: 'Marketing', count: 32},
+      {name: 'Development', count: 22},
+      {name: 'Sales', count: 17},
+      {name: 'Executive', count: 14},
+      {name: 'Admin', count: 6}
+    ];
+    if (!this.props.sidebarVisible) {
+      return (
+        <div id="sidebar">
+          <button className="toggle" onClick={this.props.sidebar}><i className="fa fa-navicon" /></button>
+        </div>
+      );
+    }
+    return (
+      <div id="sidebar">
+        <button className="toggle" onClick={this.props.sidebar}><i className="fa fa-navicon" /></button>
+        <ul>
+          {tags.map((item) => {
+            return (
+              <li>
+                <a href="">
+                  <span className="title">{item.name}</span><br />
+                  <span className="count">{item.count} posts</span>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
 });
 
