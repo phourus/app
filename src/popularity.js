ca88caef448dc40a54dca3b42e021a63d4a968d5
Jesse Drelick <info@jessedrelick.com>
2016-01-30 13:21:16 -0800

    Popularity replace red with grey

--- src/popularity.js
+++ src/popularity.js
@@ -5,22 +5,24 @@ module.exports = function (element, popularity) {
   var radius = 50;
   var posX = 90;
   var posY = element.height / 2;
-  var end = (100 - popularity) / 100;
+  var end = popularity / 100;
 
   ctx.clearRect(0, 0, element.width, element.height);
 
   ctx.font = "30px 'open sans'";
   ctx.textAlign = 'center';
   ctx.fillText(popularity + '%', posX, posY + 10);
+
   ctx.beginPath();
-  ctx.strokeStyle = '#99CC33';
+  ctx.strokeStyle = '#ddd';
   ctx.lineCap = 'square';
   ctx.lineWidth = 10;
   ctx.arc(posX, posY, radius, 0, circ, false);
+
   ctx.stroke();
 
   ctx.beginPath();
-  ctx.strokeStyle = '#AA0000';
+  ctx.strokeStyle = '#99CC33';
   ctx.lineCap = 'square';
   ctx.lineWidth = 20;
   ctx.arc(posX, posY, radius, -(quart), ((circ) * end) - quart, false);
