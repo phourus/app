e9a13a4ee69d292f9b668a00aab5329e23a125cf
Jesse Drelick <info@jessedrelick.com>
2016-01-30 20:50:32 -0800

    Server rendering

--- index.js
+++ index.js
@@ -5,36 +5,28 @@ var fs = require('fs');
 var express = require('express');
 var React = require('react');
 var ReactDOM = require('react-dom/server');
-var api = require('./api/rest');
 var Router = require('react-router');
 var match = Router.match;
-var RoutingContext = Router.RoutingContext;
 
+var api = require('./api/rest');
 var routes = require('./src/routes');
+var server = require('./src/server');
 
 var phourus = express();
 
 phourus.use(express.static(__dirname + '/build'));
 phourus.use('/rest', api);
 
-phourus.get('*', renderStatic);
+phourus.get('*', renderServer);
 
-function renderStatic (req, res) {
-  var file, app, out;
-  file = fs.readFileSync('build/app.html', 'utf8');
-  // app = React.renderToString(React.createElement(App, null));
-  // out = file.replace('$APP', ReactDOM.render(app));
-  res.send(file, 200);
-}
-
-function renderDynamic (req, res) {
+function renderServer (req, res) {
   match({ routes: routes, location: req.url }, function (error, redirectLocation, renderProps) {
     if (error) {
       //res.status(500).send(error.message)
     } else if (redirectLocation) {
       //res.redirect(302, redirectLocation.pathname + redirectLocation.search)
     } else if (renderProps) {
-      var el = React.createElement(RoutingContext, renderProps);
+      var el = React.createElement(server, renderProps);
       res.status(200).send(ReactDOM.renderToString(el));
     } else {
       //res.status(404).send('Not found')
--- src/app.js
+++ src/app.js
@@ -31,28 +31,6 @@ let App = React.createClass({
         className += " visible";
       }
     }
-    // <html lang="en">
-    //   <head>
-    //     <title>Phourus.com</title>
-    //     <meta name="Author" content="Phourus.com" />
-    //     <meta name="Description" content="A central place to manage the information, content and ideas for your organization" />
-    //     <meta name="Keywords" content="Information,Content,Ideas,Organization" />
-    //     <meta charSet="utf-8" />
-    //     <meta content="minimum-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=no" name="viewport" />
-    //     <link rel="stylesheet" href="/style.css" type="text/css" />
-    //     <link rel="stylesheet" href="/quill.base.css" type="text/css" />
-    //     <link rel="stylesheet" href="/quill.snow.css" type="text/css" />
-    //     <link rel="shortcut icon" href="/assets/favicon.ico" type="image/x-icon" />
-    //     <link rel="icon" href="/assets/favicon.ico" type="image/x-icon" />
-    //     <link href='http://fonts.googleapis.com/css?family=Rokkitt:400,700|Open+Sans:400,700' rel='stylesheet' type='text/css' />
-    //   </head>
-    //   <body className="body">
-    //     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
-    //     <script src="http://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
-    //     <script src="http://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.js"></script>
-    //     <script type="text/javascript" src="/app.js"></script>
-    //   </body>
-    // </html>
     return  (
       <div id="app">
         <Initializer />
