e8fd2376fcc8365419af286f83eb4501c2589259
Jesse Drelick <info@jessedrelick.com>
2016-02-28 15:31:30 -0800

    Fix cross storage

--- src/token.js
+++ src/token.js
@@ -2,7 +2,19 @@
 let storage = {};
 if (typeof document !== 'undefined') {
   let Client = require('cross-storage').CrossStorageClient;
-  storage = new Client('/hub.html');
+  let host = location.hostname;
+  let protocol = location.protocol + '//';
+  let port = location.port;
+  if (location.port !== "") {
+    port = `:${location.port}`;
+  }
+  let options = ['localhost', 'phourus.local', 'phourus.com', 'phourus-staging.us-west-2.elasticbeanstalk.com'];
+  for (let i = 0; i < options.length; i++) {
+    if (host.indexOf(options[i]) > -1) {
+      let url = `${protocol}${options[i]}${port}/hub.html`;
+      storage = new Client(url);
+    }
+  }
 }
 
 module.exports = storage;
