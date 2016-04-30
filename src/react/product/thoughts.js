e4bee1a116c951dbe8be400483945dd464798998
Jesse Drelick <info@jessedrelick.com>
2016-04-30 14:40:12 -0700

    Thoughts slider

--- src/react/product/thoughts.js
+++ src/react/product/thoughts.js
@@ -1,14 +1,39 @@
-let React = require('react');
+import React from 'react'
 
-module.exports = React.createClass({
-  render: function () {
+const slides = [
+  {text: 'Use Blogs and Ideas to share your general thoughts', img: '/assets/product/post.png'},
+  {text: 'Subjects and Questions are for learning and sharing knowledge', img: '/assets/product/post.png'},
+  {text: 'Debates and Polls are for polarizing discussions', img: '/assets/product/post.png'},
+  {text: 'Quotes and Opinions are for expressing yourself', img: '/assets/product/post.png'}
+]
+export default class Thoughts extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      slide: 0
+    }
+  }
+
+  render() {
+    let { slide } = this.state
+    let selected = slides[slide]
     return (
       <div className="thoughts">
         <h2>Stream your thoughts in multiple ways</h2>
-        <div className="slides"><img src="/assets/product/slider.png" /></div>
-        <p>Use blogs and ideas to share your general thoughts</p>
-        <div className="posts">
-          <img src="/assets/product/post.png" />
+        <div className="slides">
+          <ul>
+            <li onClick={this.select.bind(this, 0)} className={slide === 0 ? 'active' : ''}></li>
+            <li onClick={this.select.bind(this, 1)} className={slide === 1 ? 'active' : ''}></li>
+            <li onClick={this.select.bind(this, 2)} className={slide === 2 ? 'active' : ''}></li>
+            <li onClick={this.select.bind(this, 3)} className={slide === 3 ? 'active' : ''}></li>
+          </ul>
+        </div>
+        <div className="slide">
+          <p>{selected.text}</p>
+          <div className="posts">
+            <img src={selected.img} />
+          </div>
         </div>
         <div className="stream">
           <img src="/assets/product/thoughts.png" />
@@ -16,4 +41,8 @@ module.exports = React.createClass({
       </div>
     )
   }
-})
+
+  select(id) {
+    this.setState({slide: id})
+  }
+}
