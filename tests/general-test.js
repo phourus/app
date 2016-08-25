90f4a9a64d341a780b61ce335f5bf67af87b7b9e
Jesse Drelick <info@jessedrelick.com>
2016-08-24 22:41:38 -0700

    Remove tests/

--- tests/general-test.js
+++
@@ -1,9 +0,0 @@
-jest.dontMock('../src/react/general');
-var React, General, TestUtils;
-describe('General Test', function() {
- it('case 1', function() {
-    React = require('react/addons');
-    General = require('../src/react/general');
-    TestUtils = React.addons.TestUtils;
- });
-});
--- tests/landing-test.js
+++
@@ -1,9 +0,0 @@
-jest.dontMock('../src/react/landing');
-var React, Landing, TestUtils;
-describe('Landing Test', function() {
- it('case 1', function() {
-    React = require('react/addons');
-    Landing = require('../src/react/landing');
-    TestUtils = React.addons.TestUtils;
- });
-});
